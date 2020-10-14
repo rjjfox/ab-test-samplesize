00185e64af43348b19071ded636fbe641596477a
rorycox <ryanfox212@gmail.com>
2020-10-14 17:30:26 +0200

    Removed venv directory

--- venv/Lib/site-packages/tests/browser/README.md
+++
@@ -1,6 +0,0 @@
-Browser tests
-=============
-
-These are headless browser tests which will only work in Python 3.
-
-They should verify that all the example notebooks render without error.
--- venv/Lib/site-packages/tests/dev-containers/build-and-screenshot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PYDECK_VERSION="0.4.0"
-export PYPI_INSTALL_URL=https://test.pypi.org/simple/
-docker-compose build --force-rm --no-cache --parallel && docker-compose up --no-build -d
-python snap.py
-docker-compose down
